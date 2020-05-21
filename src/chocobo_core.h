3bafb46067f56c5692cf4a3ad1d00f0778218657
Nick Macey <nick.macey@outlook.com>
2020-05-21 16:32:21 +0100

    More hydro

--- src/chocobo_core.h
+++ src/chocobo_core.h
@@ -1,11 +1,20 @@
-// Date types
+// Data types
 #define CHINT int
 #define CHFLOAT double
+#define CHRETURN void
+
+// Array limits
+#define MAXMAT 10
 
 // Pi
 #define PI 3.14159265358979324
 #define TWOPI 2.0*3.14159265358979324
 
 // Cutoffs
-#define DTMINHG = 0.000000008
-#define DENCUT = 1.000000000E-6
+#define DTMINHG 0.000000008
+#define DENCUT 1.000000000E-6
+
+// LOOPS
+#define CELL_LOOP for (int cell=0; cell<nel; cell++)
+#define NODE_LOOP for (int node=0; node<nnod; node++)
+#define VERTEX_LOOP for (int vertex=0; vertex<4; vertex++)
--- src/hydro.c
+++ src/hydro.c
@@ -2,11 +2,11 @@
 
 #include "chocobo_core.h"
 
-void get_total_energy(CHINT nel, CHFLOAT energy[nel], CHFLOAT rho[nel],
-                      CHFLOAT u[nel], CHFLOAT v[nel], CHFLOAT mass[nel],
-                      CHFLOAT elwtc[nel][4], CHINT nodelist[nel][4],
-                      CHFLOAT *total_energy, CHFLOAT *total_ke,
-                      CHFLOAT *total_ie)
+CHRETURN get_total_energy(CHINT nel, CHFLOAT energy[nel], CHFLOAT rho[nel],
+                          CHFLOAT u[nel], CHFLOAT v[nel], CHFLOAT mass[nel],
+                          CHFLOAT elwtc[nel][4], CHINT nodelist[nel][4],
+                          CHFLOAT *total_energy, CHFLOAT *total_ke,
+                          CHFLOAT *total_ie)
 {
     *total_energy = 0.0;
     *total_ie = 0.0;
@@ -16,46 +16,48 @@ void get_total_energy(CHINT nel, CHFLOAT energy[nel], CHFLOAT rho[nel],
     CHFLOAT element_energy;
     CHINT node;
 
-    for (int i=0; i<nel; i++) {
+    CELL_LOOP {
         tek = 0.0;
-        for (int j=0; j<4; j++) {
-            node = nodelist[i][j];
-            tek += 0.5*rho[i]*elwtc[i][j]*(pow(u[node],2) + pow(v[node],2))*TWOPI;
+        VERTEX_LOOP {
+            node = nodelist[cell][vertex];
+            tek += 0.5*rho[cell]*elwtc[cell][vertex]*(
+                pow(u[node],2) + pow(v[node],2)
+            )*TWOPI;
         }
-        element_energy = mass[i]*energy[i]*TWOPI + tek;
+        element_energy = mass[cell]*energy[cell]*TWOPI + tek;
         *total_ke += tek;
-        *total_ie += mass[i]*energy[i]*TWOPI;
+        *total_ie += mass[cell]*energy[cell]*TWOPI;
         *total_energy += element_energy;
     }
 }
 
 
-void calculate_mass(
+CHRETURN calculate_mass(
     CHINT nel, CHFLOAT volume[nel], CHFLOAT rho[nel], CHFLOAT mass[nel]
 )
 {
-    for (CHINT i=0; i<nel; i++) {
-        mass[i] = volume[i]*rho[i];
+    CELL_LOOP {
+        mass[cell] = volume[cell]*rho[cell];
     }
 }
 
 
-void calculate_finite_elements(CHINT nel, CHFLOAT x[nel], CHFLOAT y[nel],
-                               CHINT nodelist[nel][4], CHFLOAT ni[nel][4],
-                               CHFLOAT dndx[nel][4], CHFLOAT dndy[nel][4],
-                               CHFLOAT pdndx[nel][4], CHFLOAT pdndy[nel][4],
-                               CHFLOAT elwtc[nel][4])
+CHRETURN calculate_finite_elements(CHINT nel, CHFLOAT x[nel], CHFLOAT y[nel],
+                                   CHINT nodelist[nel][4], CHFLOAT ni[nel][4],
+                                   CHFLOAT dndx[nel][4], CHFLOAT dndy[nel][4],
+                                   CHFLOAT pdndx[nel][4], CHFLOAT pdndy[nel][4],
+                                   CHFLOAT elwtc[nel][4])
 {
     CHFLOAT a1, a2, a3;
     CHFLOAT b1, b2, b3;
     CHFLOAT jacobian;
     CHINT n1, n2, n3, n4;
 
-    for (int i=0; i<nel; i++) {
-        n1 = nodelist[i][0];
-        n2 = nodelist[i][1];
-        n3 = nodelist[i][2];
-        n4 = nodelist[i][3];
+    CELL_LOOP {
+        n1 = nodelist[cell][0];
+        n2 = nodelist[cell][1];
+        n3 = nodelist[cell][2];
+        n4 = nodelist[cell][3];
 
         a1 = 0.25*(-x[n1] + x[n2] + x[n3] - x[n4]);
         a2 = 0.25*(x[n1] - x[n2] + x[n3] - x[n4]);
@@ -66,32 +68,81 @@ void calculate_finite_elements(CHINT nel, CHFLOAT x[nel], CHFLOAT y[nel],
         b3 = 0.25*(-y[n1] - y[n2] + y[n3] + y[n4]);
 
         // Used for momentum update
-        ni[i][0] = ((3.0*b3 - b2)*(3.0*a1 - a2) - (3.0*a3 - a2)*(3.0*b1 - b2))/9.0;
-        ni[i][1] = ((3.0*b3 + b2)*(3.0*a1 - a2) - (3.0*a3 + a2)*(3.0*b1 - b2))/9.0;
-        ni[i][2] = ((3.0*b3 + b2)*(3.0*a1 + a2) - (3.0*a3 + a2)*(3.0*b1 + b2))/9.0;
-        ni[i][3] = ((3.0*b3 - b2)*(3.0*a1 + a2) - (3.0*a3 - a2)*(3.0*b1 + b2))/9.0;
+        ni[cell][0] = ((3.0*b3 - b2)*(3.0*a1 - a2) - (3.0*a3 - a2)*(3.0*b1 - b2))/9.0;
+        ni[cell][1] = ((3.0*b3 + b2)*(3.0*a1 - a2) - (3.0*a3 + a2)*(3.0*b1 - b2))/9.0;
+        ni[cell][2] = ((3.0*b3 + b2)*(3.0*a1 + a2) - (3.0*a3 + a2)*(3.0*b1 + b2))/9.0;
+        ni[cell][3] = ((3.0*b3 - b2)*(3.0*a1 + a2) - (3.0*a3 - a2)*(3.0*b1 + b2))/9.0;
 
         // Integrated d(dndx) and d(dndy) for intdiv and energy
-        dndx[i][0] = -b3 + b1;
-        dndx[i][1] = b3 + b1;
-        dndx[i][2] = b3 - b1;
-        dndx[i][3] = -b3 - b1;
+        dndx[cell][0] = -b3 + b1;
+        dndx[cell][1] = b3 + b1;
+        dndx[cell][2] = b3 - b1;
+        dndx[cell][3] = -b3 - b1;
 
-        dndy[i][0] = a3 - a1;
-        dndy[i][1] = -a3 - a1;
-        dndy[i][2] = -a3 + a1;
-        dndy[i][3] = a3 + a1;
+        dndy[cell][0] = a3 - a1;
+        dndy[cell][1] = -a3 - a1;
+        dndy[cell][2] = -a3 + a1;
+        dndy[cell][3] = a3 + a1;
 
         // Jacobian and d(dndx), d(dndy) for div and viscosity
         jacobian = a1*b3 - a3*b1;
-        for (int j=0; j<4; j++) {
-            pdndx[i][j] = 0.25*dndx[i][j]/jacobian;
-            pdndy[i][j] = 0.25*dndy[i][j]/jacobian;
+        VERTEX_LOOP {
+            pdndx[cell][vertex] = 0.25*dndx[cell][vertex]/jacobian;
+            pdndy[cell][vertex] = 0.25*dndy[cell][vertex]/jacobian;
         }
 
         // Copy ni into elwtc
-        for (int j=0; j<4; j++) {
-            elwtc[i][j] = ni[i][j];
+        VERTEX_LOOP {
+            elwtc[cell][vertex] = ni[cell][vertex];
+        }
+    }
+}
+
+
+CHRETURN calculate_div_v(CHINT nel, CHFLOAT u[nel], CHFLOAT v[nel],
+                         CHFLOAT pdndx[nel][4], CHFLOAT pdndy[nel][4],
+                         CHINT nodelist[nel][4], CHFLOAT divvel[nel])
+{
+    CHINT n;
+
+    CELL_LOOP {
+        divvel[cell] = 0.0;
+    }
+
+    CELL_LOOP {
+        VERTEX_LOOP {
+            n = nodelist[cell][vertex];
+            divvel[cell] += u[n]*pdndx[cell][vertex] + v[n]*pdndy[cell][vertex];
+        }
+    }
+}
+
+
+CHRETURN calculate_soundspeed(CHINT nel, CHFLOAT pressure[nel], CHFLOAT rho[nel],
+                              CHINT material[nel], CHFLOAT gamma[MAXMAT],
+                              CHFLOAT soundspeed[nel])
+{
+    CHINT mat;
+
+    CELL_LOOP {
+        mat = material[cell];
+        soundspeed[cell] = sqrt(gamma[mat]*pressure[cell]/rho[cell]);
+    }
+}
+
+CHRETURN calculate_q(CHINT nel, CHFLOAT rho[nel], CHFLOAT soundspeed[nel],
+                     CHFLOAT divvel[nel], CHFLOAT area[nel], CHFLOAT cq,
+                     CHFLOAT cl, CHFLOAT q[nel])
+{
+    CHFLOAT dudx;
+
+    CELL_LOOP {
+        if (divvel[cell] < 0.0) {
+            dudx = sqrt(area[cell])*divvel[cell];
+            q[cell] = cq*rho[cell]*dudx*dudx +
+                      cl*rho[cell]*soundspeed[cell]*fabs(dudx);
+        } else {
+            q[cell] = 0.0;
         }
     }
 }
